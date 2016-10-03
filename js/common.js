9aa689dcd1591df63f8e242af3f2427459905f73
Dmytro Spektoruk <dima_sp2@ro.ru>
2016-10-03 04:40:03 +0300

    just for commit

--- js/common.js
+++ js/common.js
@@ -1,34 +1,40 @@
 $(document).ready(function() {
 
 
-		function heightDetect(){
+	function heightDetect(){
 		if($(window).width()>765){
-		if($(window).height()>800){
-		$(".l_side").css('height', $(window).height());}
-		else{
-			$(".l_side").css('height', 800);
-		}
-	}};
-	heightDetect();
-	$(window).resize(function(){
-		heightDetect();
-	});
+			if($(window).height()>900){
+				$(".container .l_side").css('height', $(window).height());
+				$(".container .m_posts").css('height', $(window).height());}
+				else{
+					$(".container .l_side").css('height', 1050);
+					$(".container .m_posts").css('height', 1050);
+				}
+			}};
+			heightDetect();
+			$(window).resize(function(){
+				heightDetect();
+			});
 
 
-	$(".l_side li").click(function() {
-		 $(".current_mnu").removeClass("current_mnu");
-	  $(this).addClass("current_mnu");
-	});
+			$(".l_side li").click(function() {
+				$(".current_mnu").removeClass("current_mnu");
+				$(this).addClass("current_mnu");
+			});
 
-	$(".post a").click(function() {
-		$(".p_current").removeClass("p_current");
-	  $(this).parents(".post").addClass("p_current");
-	});
+			$(".post a").click(function() {
+				$(".p_current").removeClass("p_current");
+				$(this).parents(".post").addClass("p_current");
+			});
 
+			$(".content .con_pic").mouseover(function() {
+				$(this).children(".img_det").css('display', "block");
+			});
+			$(".content .con_pic").mouseout(function() {
+				$(this).children(".img_det").css('display', "none");
+			});
 
-
-
-});
+		});
 $(window).load(function() {
 
 	$(".loader_inner").fadeOut();
